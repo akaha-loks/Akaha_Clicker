2e7b304c5c9bf4c34abf5c7cc971cc6092caa844
Akaha LoKs <132211327+akaha-loks@users.noreply.github.com>
2023-10-11 23:18:04 +0600

    Добавил главное меню и графику
    Было добавлено главное меню, где вся графика наконец была установлена. Так же относится и к сцене с геймплейем(вся графика установлена). Теперь на игру можно смотреть не с отвращением.

---
+++ Akaha's Clicker/Assets/Scripts/UI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+   public void LoadScenes(int scenes)
+    {
+        SceneManager.LoadScene(scenes);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
